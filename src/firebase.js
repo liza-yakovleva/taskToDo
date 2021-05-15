e32c0ca4a04c43355ec41798eb8cfb29d25ce548
Elizaveta <lizayakovleva2005@gmail.com>
2021-05-15 12:26:03 +0300

    add all

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,13 @@ import 'firebase/firebase-database'
 import 'firebase/firebase-auth'
 
 const config = {
-  apiKey: "AIzaSyAVUXt93LVrzZuV-kI4MFqxf7ldmGPl3_Q",
-  authDomain: "taskstodolist-5eb01.firebaseapp.com",
-  databaseURL: "https://taskstodolist-5eb01-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "taskstodolist-5eb01",
-  storageBucket: "taskstodolist-5eb01.appspot.com",
-  messagingSenderId: "947885205618",
-  appId: "1:947885205618:web:a035dee4c223b1d97cbaeb",
-  measurementId: "G-L1LZZ48CRG"
+  apiKey: "AIzaSyAac5ugk8ldTQP0JzBChmgUprM9pLlhZs8",
+  authDomain: "tasktodo-1b417.firebaseapp.com",
+  databaseURL: "https://tasktodo-1b417-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tasktodo-1b417",
+  storageBucket: "tasktodo-1b417.appspot.com",
+  messagingSenderId: "457021017974",
+  appId: "1:457021017974:web:4f85290b0d8bb330eab457"
 }
  
 firebase.initializeApp(config)
